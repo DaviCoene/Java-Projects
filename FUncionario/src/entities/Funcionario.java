29b72a1c6c82557df6cd84820f339c3778ba3e1f
Davi Coene Rosa <62859036+DaviCoene@users.noreply.github.com>
2024-04-30 18:57:57 -0300

    Add files via upload

--- FUncionario/src/entities/Funcionario.java
+++ FUncionario/src/entities/Funcionario.java
@@ -10,11 +10,17 @@ public class Funcionario {
 		
 	}
 
-	public Funcionario(String nome, String sobrenome) {
+	
+	
+	public Funcionario(String nome, String sobrenome, Integer horasTrabalhadas, double valorPorHora) {
 		this.nome = nome;
 		this.sobrenome = sobrenome;
+		this.horasTrabalhadas = horasTrabalhadas;
+		this.valorPorHora = valorPorHora;
 	}
-	
+
+
+
 	public String nomeCompleto() {
 		return nome + sobrenome;
 	}
@@ -26,4 +32,46 @@ public class Funcionario {
 	public void incrementarHoras(Integer horas) {
 		horasTrabalhadas = horasTrabalhadas + horas; 
 	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+
+
+	public Integer getHorasTrabalhadas() {
+		return horasTrabalhadas;
+	}
+
+
+
+
+	public double getValorPorHora() {
+		return valorPorHora;
+	}
+
+
+
+	
+	
 }
--- FUncionario/src/program.java
+++ FUncionario/src/program.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
+import entities.Funcionario;
 
 public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		String nome = sc.nextLine();
+		String sobrenome = sc.nextLine();
+		int horas = sc.nextInt();
+		double valor = sc.nextDouble();
+		Funcionario colaborador = new Funcionario(nome, sobrenome, horas, valor);
+		
+		System.out.println(colaborador.nomeCompleto());
+		System.out.println(colaborador.calcularSalario());
+		
+		horas = sc.nextInt();
+		
+		colaborador.incrementarHoras(horas);
+		System.out.println(colaborador.calcularSalario());
+		
+		sc.close();
+		
 	}
 
 }
